4fe0e362c243da3b7f51423aea752798c3d808ab
Alexandre Castro <alex.monteiro@seznam.cz>
2018-09-09 21:31:48 +0200

    search function finished

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 
 
 This is an app that searches for lyrics using a 3rd party Rest API.
+
+This was built following Brad Traversy's [tutorial](https://www.youtube.com/watch?v=NDEt0KdDbhk).
+
+## NPM Scripts
+
+```
+# Installing dependencies
+npm install 
+
+# Run on local server (port 3000)
+npm start
+
+# Build for production
+npm run build
+```
--- src/components/tracks/Track.js
+++ src/components/tracks/Track.js
@@ -9,12 +9,12 @@ const Track = (props) => {
                 <div className="card-body">
                     <h5>{track.artist_name}</h5>
                     <p className="card-text">
-                        <strong><i className="fas fa-play"></i>Track</strong>: {track.track_name}
+                        <strong><i className="fas fa-play"></i> Track</strong>: {track.track_name}
                         <br/>
-                        <strong><i className="fas fa-play"></i>Album</strong>: {track.album_name}
+                        <strong><i className="fas fa-compact-disc"></i> Album</strong>: {track.album_name}
                     </p>
                     <Link to={`lyrics/track/${track.track_id}`} className="btn btn-dark btn-block">
-                        <i className="fas fa-chevron-right"></i>View Lyrics
+                        <i className="fas fa-chevron-right"></i> View Lyrics
                     </Link>
                 </div>
             </div>
