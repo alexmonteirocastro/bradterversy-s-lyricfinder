ff08f064d2e49fbfb78e7ab8b13d958cd48e588e
Alexandre Castro <alex.monteiro@seznam.cz>
2018-09-09 16:11:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Lyric search App
+
+
+This is an app that searches for lyrics using a 3rd party Rest API.
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+
+import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
+import { Provider } from './context';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <Router>
+          <React.Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Index}></Route>
+              </Switch>
+            </div>
+          </React.Fragment>
+        </Router>
+      </Provider>
+    );
+  }
+}
+
+export default App;
